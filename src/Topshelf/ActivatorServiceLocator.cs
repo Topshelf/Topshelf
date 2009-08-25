03675fc509f62f665b58d38c66d93a2fd9cbaeb4
Dru Sellers <dru@drusellers.com>
2009-08-25 21:28:50 +0000

    adding ActivatorServiceLocator.cs

---
+++ src/Topshelf/ActivatorServiceLocator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Practices.ServiceLocation;
+
+namespace Topshelf
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ActivatorServiceLocator :
+        IServiceLocator
+    {
+        public object GetService(Type serviceType)
+        {
+            return Activator.CreateInstance(serviceType);
+        }
+
+        public object GetInstance(Type serviceType)
+        {
+            return Activator.CreateInstance(serviceType);
+        }
+
+        public object GetInstance(Type serviceType, string key)
+        {
+            return Activator.CreateInstance(serviceType);
+        }
+
+        public IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return new []{Activator.CreateInstance(serviceType)};
+        }
+
+        public TService GetInstance<TService>()
+        {
+            return (TService)Activator.CreateInstance(typeof(TService));
+        }
+
+        public TService GetInstance<TService>(string key)
+        {
+            return (TService)Activator.CreateInstance(typeof(TService));
+        }
+
+        public IEnumerable<TService> GetAllInstances<TService>()
+        {
+            return new[] { (TService)Activator.CreateInstance(typeof(TService)) };
+        }
+    }
+}
