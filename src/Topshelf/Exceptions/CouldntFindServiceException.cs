87cecf4b7deeb07826f3b6e83180eeff945c03d6
Dru Sellers <dru@drusellers.com>
2009-12-24 20:05:08 +0000

    exceptions should carry the [Serializable] attribute

--- src/Topshelf/Exceptions/CouldntFindServiceException.cs
+++ src/Topshelf/Exceptions/CouldntFindServiceException.cs
@@ -3,6 +3,7 @@ namespace Topshelf.Exceptions
     using System;
     using System.Runtime.Serialization;
 
+    [Serializable]
     public class CouldntFindServiceException : 
         Exception
     {
