7d7804e2c8558090a11871b6af36bfcca67495fd
Dru Sellers <dru@drusellers.com>
2010-05-19 10:31:09 -0500

    adding chris's better where clause

--- src/Topshelf/Shelving/Shelf.cs
+++ src/Topshelf/Shelving/Shelf.cs
@@ -70,7 +70,7 @@ namespace Topshelf.Shelving
         {
             if (bootstrapper != null)
             {
-                if (bootstrapper.GetInterfaces().Where(x => x.Name == typeof(Bootstrapper<>).Name && x.Namespace == typeof(Bootstrapper<>).Namespace).Count() > 0)
+                if (bootstrapper.GetInterfaces().Where(x =>x.GetGenericTypeDefinition() == typeof(Bootstrapper<>)).Count() > 0)
                     return bootstrapper;
 
                 throw new InvalidOperationException("Bootstrapper type, " + bootstrapper.GetType().Name
@@ -80,7 +80,7 @@ namespace Topshelf.Shelving
             var possibleTypes = AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(x => x.GetTypes())
                 .Where(x => x.IsInterface == false)
-                .Where(t => t.GetInterfaces().Any(i => i.Name.StartsWith("Bootstrapper")));
+                .Where(t => t.GetInterfaces().Any(i => i.GetGenericTypeDefinition() == typeof(Bootstrapper<>)));
 
             if (possibleTypes.Count() > 1)
                 throw new InvalidOperationException("Unable to identify the bootstrapper, more than one found.");
