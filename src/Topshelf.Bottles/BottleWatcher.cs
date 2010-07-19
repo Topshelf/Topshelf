df7dd33512e08487fafe329139abe44428fd4197
Dru Sellers <dru@drusellers.com>
2010-07-19 09:22:46 -0500

    adding bottles

---
+++ src/Topshelf.Bottles/BottleWatcher.cs
@@ -0,0 +1,61 @@
+﻿// Copyright 2007-2010 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Topshelf.Bottles
+{
+    using System;
+    using System.Collections.Generic;
+    using Magnum.Channels;
+    using Magnum.Extensions;
+    using Magnum.FileSystem;
+    using Magnum.FileSystem.Events;
+    using Magnum.FileSystem.Zip;
+
+    public class BottleWatcher
+    {
+        readonly FileSystemEventProducerFactory _factory;
+        ChannelAdapter _eventChannel;
+        Action<Directory> _actionToTake;
+
+        public BottleWatcher()
+        {
+            _factory = new FileSystemEventProducerFactory();
+        }
+
+        public List<IDisposable> Watch(string directoryToWatch, Action<Directory> actionToTake)
+        {
+            _actionToTake = actionToTake;
+            _eventChannel = new ChannelAdapter();
+            _eventChannel.Connect(x => x.AddConsumerOf<FileCreated>().UsingConsumer(ProcessNewFile));
+            var listeners = _factory.CreateFileSystemEventProducers(directoryToWatch, true, true, _eventChannel, 1.Minutes());
+            return listeners;
+        }
+
+        void ProcessNewFile(FileCreated message)
+        {
+            if (message.Path.EndsWith("bottle"))
+            {
+                //TODO: is this ok - the null?
+                Directory dir = new ZippedDirectory(DirectoryName.GetDirectoryName(message.Path), null);
+                try
+                {
+                    _actionToTake(dir);
+                }
+                catch (Exception ex)
+                {
+                    string msg = "There was an error processing the bottle '{0}'".FormatWith(message.Path);
+                    throw new BottleException(msg, ex);
+                }
+            }
+        }
+    }
+}
---
+++ src/Topshelf/Shelving/WellknownChannels.cs
@@ -0,0 +1,59 @@
+﻿// Copyright 2007-2008 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Topshelf.Shelving
+{
+    using System;
+    using System.Diagnostics;
+    using Magnum.Channels;
+    using Magnum.Fibers;
+
+    public static class WellknownChannels
+    {
+        static Uri _baseUri = new Uri("net.pipe://localhost/topshelf");
+
+        public static UntypedChannel CurrentShelfChannel
+        {
+            get { return new WcfUntypedChannelProxy(new ThreadPoolFiber(), _baseUri, "{0}-{1}".FormatWith(GetFolder(), GetPid())); }
+        }
+
+        public static UntypedChannel HostChannel
+        {
+            get { return new WcfUntypedChannelProxy(new ThreadPoolFiber(), _baseUri, "host-{0}".FormatWith(GetPid())); }
+        }
+
+        public static UntypedChannel GetShelfAddress(AppDomain appDomain)
+        {
+            return new WcfUntypedChannelProxy(new ThreadPoolFiber(), _baseUri, "{0}-{1}".FormatWith(appDomain.FriendlyName, GetPid()));
+        }
+
+        public static WcfUntypedChannelHost BuildChannelHostForHost(UntypedChannelAdapter adapter)
+        {
+            return new WcfUntypedChannelHost(new SynchronousFiber(), adapter, new Uri("net.pipe://localhost/topshelf"), "host-{0}".FormatWith(GetPid()));
+        }
+
+        public static WcfUntypedChannelHost BuildChannelHostForShelf(UntypedChannelAdapter adapter)
+        {
+            return new WcfUntypedChannelHost(new ThreadPoolFiber(), adapter, new Uri("net.pipe://localhost/topshelf"), "{0}-{1}".FormatWith(GetFolder(), GetPid()));
+        }
+
+        public static int GetPid()
+        {
+            return Process.GetCurrentProcess().Id;
+        }
+
+        public static string GetFolder()
+        {
+            return AppDomain.CurrentDomain.FriendlyName;
+        }
+    }
+}
