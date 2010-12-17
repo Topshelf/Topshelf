c0298826403c1cebbc4b84a820b81e78423fb245
Travis Smith <travis@legomaster.net>
2010-12-16 20:20:20 -0500

    A resolution for issue#16. Null reference won't happen any more but it might not be the most ideal resolution. Considing adding a validation step to creation of the runner configuration but it might add more steps for people who won't actually run it as a service.

--- src/Topshelf/Configuration/WinServiceSettings.cs
+++ src/Topshelf/Configuration/WinServiceSettings.cs
@@ -1,4 +1,4 @@
-// Copyright 2007-2008 The Apache Software Foundation.
+// Copyright 2007-2010 The Apache Software Foundation.
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -23,6 +23,10 @@ namespace Topshelf.Configuration
 		{
 			StartMode = ServiceStartMode.Automatic;
 			Dependencies = new List<string>();
+
+			Description = string.Empty;
+			DisplayName = string.Empty;
+			ServiceName = new ServiceName(string.Empty);
 		}
 
 		public ServiceStartMode StartMode { get; set; }
