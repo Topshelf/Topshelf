e066fa041808b6e55b03475ed9e60223ed2a0186
Dru Sellers <dru@drusellers.com>
2012-03-16 21:11:46 -0500

    removing bottles code for now.
    need to focus on process hosting before bottles
    
    also need to remember bottles work w/ topsheld in milkman

--- src/Topshelf.Specs/AppDomain_Specs.cs
+++ src/Topshelf.Specs/AppDomain_Specs.cs
@@ -1,7 +1,7 @@
-// Copyright 2007-2010 The Apache Software Foundation.
+// Copyright 2007-2012 The Apache Software Foundation.
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
-// this file except in compliance with the License. You may obtain a copy of the 
+// his file except in compliance with the License. You may obtain a copy of the 
 // License at 
 // 
 //     http://www.apache.org/licenses/LICENSE-2.0 
@@ -10,109 +10,109 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
+
 namespace Topshelf.Specs
 {
-	using System;
-	using System.IO;
-	using System.Threading;
-	using log4net;
-	using Magnum.Extensions;
-	using Magnum.TestFramework;
-	using Messages;
-	using Model;
-	using NUnit.Framework;
-	using Shelving;
-	using Topshelf.Configuration.Dsl;
+    using System;
+    using System.IO;
+    using System.Threading;
+    using Configuration.Dsl;
+    using Magnum.Extensions;
+    using Messages;
+    using Model;
+    using NUnit.Framework;
+    using Shelving;
+    using log4net;
 
 
-	[TestFixture]
-	[Explicit]
-	public class Using_the_shelf_service_controller_to_start_a_service
-	{
-		[SetUp]
-		public void Setup()
-		{
-			if (Directory.Exists("Services"))
-				Directory.Delete("Services", true);
+    [TestFixture]
+    [Explicit]
+    public class Using_the_shelf_service_controller_to_start_a_service
+    {
+        [SetUp]
+        public void Setup()
+        {
+            if (Directory.Exists("Services"))
+                Directory.Delete("Services", true);
 
-			Directory.CreateDirectory("Services");
-			string bobPath = Path.Combine("Services", "bob");
-			Directory.CreateDirectory(bobPath);
+            Directory.CreateDirectory("Services");
+            string bobPath = Path.Combine("Services", "bob");
+            Directory.CreateDirectory(bobPath);
 
-			DirectoryMonitor_Specs.CopyFileToDir("TopShelf.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("TopShelf.Specs.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("Magnum.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("Magnum.FileSystem.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("Ionic.Zip.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("Newtonsoft.Json.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("System.Threading.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("log4net.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("Stact.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("Stact.serverframework.dll", bobPath);
-			DirectoryMonitor_Specs.CopyFileToDir("Spark.dll", bobPath);
-		}
+            DirectoryMonitor_Specs.CopyFileToDir("TopShelf.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("TopShelf.Specs.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("Magnum.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("Magnum.FileSystem.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("Ionic.Zip.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("Newtonsoft.Json.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("System.Threading.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("log4net.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("Stact.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("Stact.serverframework.dll", bobPath);
+            DirectoryMonitor_Specs.CopyFileToDir("Spark.dll", bobPath);
+        }
 
-		[TearDown]
-		public void CleanUp()
-		{
-			//Directory.Delete("Services", true);
-		}
+        [TearDown]
+        public void CleanUp()
+        {
+            //Directory.Delete("Services", true);
+        }
 
-		[Test]
-		public void Should_start_the_shelf_in_the_separate_app_domain()
-		{
-			_log.Debug("Starting up the controller");
+        [Test]
+        public void Should_start_the_shelf_in_the_separate_app_domain()
+        {
+            _log.Debug("Starting up the controller");
 
-			using (var coordinator = new Model.ServiceCoordinator())
-			{
-				coordinator.Start();
-				coordinator.Send(new CreateShelfService("bob", ShelfType.Folder, typeof(TestAppDomainBootsrapper)));
+            using (var coordinator = new Model.ServiceCoordinator())
+            {
+                coordinator.Start();
+                coordinator.Send(new CreateShelfService("bob", ShelfType.Folder, typeof(TestAppDomainBootsrapper)));
 
-				TestAppDomainBootsrapper.Started.WaitOne(20.Seconds()).ShouldBeTrue();
-			}
-		}
+                TestAppDomainBootsrapper.Started.WaitOne(20.Seconds()).ShouldBeTrue();
+            }
+        }
 
-		static readonly ILog _log = LogManager.GetLogger(typeof(Using_the_shelf_service_controller_to_start_a_service));
-	}
+        static readonly ILog _log = LogManager.GetLogger(typeof(Using_the_shelf_service_controller_to_start_a_service));
+    }
 
 
-	public class TestAppDomainBootsrapper :
-		Bootstrapper<object>
-	{
-		[ThreadStatic]
-		static Semaphore _started;
+    public class TestAppDomainBootsrapper :
+        Bootstrapper<object>
+    {
+        [ThreadStatic]
+        static Semaphore _started;
 
-		[ThreadStatic]
-		static Semaphore _stopped;
+        [ThreadStatic]
+        static Semaphore _stopped;
 
-		public static Semaphore Started
-		{
-			get
-			{
-				if (_started == null)
-					_started = new Semaphore(0, 100, "TestAppDomainBootstrapperSemaphore");
+        public static Semaphore Started
+        {
+            get
+            {
+                if (_started == null)
+                    _started = new Semaphore(0, 100, "TestAppDomainBootstrapperSemaphore");
 
-				return _started;
-			}
-		}
+                return _started;
+            }
+        }
 
-		public static Semaphore Stopped
-		{
-			get
-			{
-				if (_stopped == null)
-					_stopped = new Semaphore(0, 100, "TestAppDomainBootstrapperSemaphore");
+        public static Semaphore Stopped
+        {
+            get
+            {
+                if (_stopped == null)
+                    _stopped = new Semaphore(0, 100, "TestAppDomainBootstrapperSemaphore");
 
-				return _stopped;
-			}
-		}
+                return _stopped;
+            }
+        }
 
-		public void InitializeHostedService(IServiceConfigurator<object> cfg)
-		{
-			cfg.HowToBuildService(serviceBuilder => new object());
+        public void InitializeHostedService(IServiceConfigurator<object> cfg)
+        {
+            cfg.HowToBuildService(serviceBuilder => new object());
 
-			cfg.WhenStarted(a => { Started.Release(); });
-			cfg.WhenStopped(a => { Stopped.Release(); });
-		}
-	}
+            cfg.WhenStarted(a => { Started.Release(); });
+            cfg.WhenStopped(a => { Stopped.Release(); });
+        }
+    }
 }
