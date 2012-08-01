d1cf55f6f22cd61a7707628b88cf98b480744398
Jeremy Wiebe <jeremy.wiebe@gmail.com>
2012-08-01 09:19:32 -0400

    Fix ilmerge when spaces in project path

--- build_support/ilmerge.rb
+++ build_support/ilmerge.rb
@@ -25,9 +25,9 @@ class ILMerge
 
   def execute
     params = []
-    params << "/out:#{@output}" unless @output.nil?
-	params << "/log:#{@log}" unless @log.nil?
-	params << "/internalize:#{@internalize}" unless @internalize.nil?
+    params << "/out:\"#{@output}\"" unless @output.nil?
+    params << "/log:\"#{@log}\"" unless @log.nil?
+    params << "/internalize:\"#{@internalize}\"" unless @internalize.nil?
     params << "/target:#{@target}" unless @target.nil?
     params << "/allowDup" if @allow_dupes
     params << "/xmldocs" unless @no_docs
