ff919ec0151d4202603a65a9945093d3c56a4ec3
Henrik <henrik@haf.se>
2012-09-05 01:32:37 +0200

    first steps..

--- samples/manifests/mono.pp
+++ samples/manifests/mono.pp
@@ -1,2 +1,19 @@
 node 'mono.local' {
+  $sentinel = "/var/lib/apt/first-puppet-run"
+  exec { "apt-get update":
+    command => "/usr/bin/apt-get update && touch ${sentinel}",
+    onlyif => "/usr/bin/env test \\! -f ${sentinel} || /usr/bin/env test \\! -z \"$(find /etc/apt -type f -c
+newer ${sentinel})\"",
+    timeout => 3600,
+    notify => Exec['apt-get upgrade']
+  }
+  exec { "apt-get upgrade":
+    command => "/usr/bin/apt-get upgrade -y",
+    refreshonly => true
+  }
+  package {
+    ['mono-runtime', 'mono-4.0-service']:
+    ensure  => present,
+    require => Exec['apt-get upgrade']
+  }
 }
