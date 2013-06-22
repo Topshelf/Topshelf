a2902d87c431d5cf659746268c4505bea76ab3ac
Travis Smith <travis.smith@mckesson.com>
2013-06-22 10:56:29 -0400

    Initial stab at using Assembly for service info

--- src/SampleTopshelfService/Program.cs
+++ src/SampleTopshelfService/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+﻿// Copyright 2007-2013 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -21,6 +21,8 @@ namespace SampleTopshelfService
             return (int)HostFactory.Run(x =>
                 {
                     x.UseLog4Net("log4net.config");
+
+                    x.UseAssemblyInfoForServiceInfo();
 
                     bool throwOnStart = false;
                     bool throwOnStop = false;
