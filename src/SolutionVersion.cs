c736e5b7c489a7045059b495df18b7c6413bc2d7
Chris Patterson <chris@phatboyg.com>
2013-06-24 09:31:51 -0700

    Updated internals to latest version and incremented version

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Topshelf is an open source project for hosting services without friction. By referencing Topshelf, your console application *becomes* a service installer with a comprehensive set of command-line options for installing, configuring, and running your application as a service.")]
 [assembly: AssemblyProduct("Topshelf")]
 [assembly: AssemblyCopyright("Copyright 2012 Chris Patterson, Dru Sellers, Travis Smith, All rights reserved.")]
-[assembly: AssemblyVersion("3.1.1")]
-[assembly: AssemblyFileVersion("3.1.1")]
+[assembly: AssemblyVersion("3.1.2")]
+[assembly: AssemblyFileVersion("3.1.2")]
 
-[assembly: AssemblyInformationalVersion("3.1.1.93ec9c")]
+[assembly: AssemblyInformationalVersion("3.1.2.a2902d")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
 
