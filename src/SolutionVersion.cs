75cb4dc8b27daf99a184e2972bfd961619ad96b9
Chris Patterson <chris@phatboyg.com>
2015-03-11 16:37:05 -0700

    Update Gemfile

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Topshelf is an open source project for hosting services without friction. By referencing Topshelf, your console application *becomes* a service installer with a comprehensive set of command-line options for installing, configuring, and running your application as a service.")]
 [assembly: AssemblyProduct("Topshelf")]
 [assembly: AssemblyCopyright("Copyright 2012 Chris Patterson, Dru Sellers, Travis Smith, All rights reserved.")]
-[assembly: AssemblyVersion("3.1.4")]
-[assembly: AssemblyFileVersion("3.1.4")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
 
-[assembly: AssemblyInformationalVersion("3.1.4.f2c4c1")]
+[assembly: AssemblyInformationalVersion("3.2.0.59fd78")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
