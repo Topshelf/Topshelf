a55f93f7a590067d90d6203ab034cf9d438925f4
gal.halup <gal.halup@sealights.io>
2018-01-14 14:38:33 +0200

    Support service recovery configuration times in TimeSpan format

--- src/Topshelf/Configuration/ServiceRecoveryConfigurator.cs
+++ src/Topshelf/Configuration/ServiceRecoveryConfigurator.cs
@@ -17,8 +17,21 @@ namespace Topshelf
         /// <summary>
         ///   Restart the service after waiting the delay period specified
         /// </summary>
+        /// <param name="delay"></param>
+        ServiceRecoveryConfigurator RestartService(System.TimeSpan delay);
+
+        /// <summary>
+        ///   Restart the service after waiting the delay period in minutes
+        /// </summary>
         /// <param name="delayInMinutes"> </param>
         ServiceRecoveryConfigurator RestartService(int delayInMinutes);
+
+        /// <summary>
+        ///   Restart the computer after waiting the delay period specified
+        /// </summary>
+        /// <param name="delay"></param>
+        /// <param name="message"></param>
+        ServiceRecoveryConfigurator RestartComputer(System.TimeSpan delay, string message);
 
         /// <summary>
         ///   Restart the computer after waiting the delay period in minutes
@@ -26,6 +39,13 @@ namespace Topshelf
         /// <param name="delayInMinutes"> </param>
         /// <param name="message"> </param>
         ServiceRecoveryConfigurator RestartComputer(int delayInMinutes, string message);
+
+        /// <summary>
+        ///   Run the command specified
+        /// </summary>
+        /// <param name="delay"></param>
+        /// <param name="command"> </param>
+        ServiceRecoveryConfigurator RunProgram(System.TimeSpan delay, string command);
 
         /// <summary>
         ///   Run the command specified
