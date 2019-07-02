3e2d27a50a4ccca52fc6d151f7fa10d3e232583a
Chris Patterson <chris@phatboyg.com>
2019-07-02 08:48:54 -0500

    updated version

--- build.fsx
+++ build.fsx
@@ -11,7 +11,7 @@ let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./Topshelf.snk"
 
 let assemblyVersion = "4.0.0.0"
-let baseVersion = "4.2.0"
+let baseVersion = "4.2.1"
 
 let envVersion = (environVarOrDefault "APPVEYOR_BUILD_VERSION" (baseVersion + ".0"))
 let buildVersion = (envVersion.Substring(0, envVersion.LastIndexOf('.')))
