d95ae26f5bac0ef8b16c80da0102415670bf7306
SilverFox <yyjdelete@126.com>
2020-04-29 11:58:49 +0800

    Remove some conditional compile for NETCORE
    Fix #568

--- src/Topshelf/Configuration/Options/ServiceRecoveryOption.cs
+++ src/Topshelf/Configuration/Options/ServiceRecoveryOption.cs
@@ -10,7 +10,6 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-#if !NETCORE
 namespace Topshelf.Options
 {
     using Topshelf.HostConfigurators;
@@ -69,4 +68,3 @@ namespace Topshelf.Options
         }
     }
 }
-#endif
